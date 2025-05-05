83368032f28341095ee00c3a0af9b421973be110
pedronns <pedro.dnunes1@gmail.com>
2025-05-05 15:10:57 -0300

    Repondo os arquivos

---
+++ routes/web.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TesteController;
+use App\Http\Controllers\FornecedorController;
+
+use App\Http\Middleware\LogAcessoMiddleware;
+
+/*
+Route::get('/', function () {
+    return 'Olá';
+});
+ */
+
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/',
+    [\App\Http\Controllers\PrincipalController::class,
+    'principal'
+])->name('site.index');
+
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/sobre-nos',
+    [\App\Http\Controllers\SobreNosController::class,
+    'sobreNos'
+])->name('site.sobrenos');
+
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/contato',
+    [\App\Http\Controllers\ContatoController::class,
+    'contato'
+])->name('site.contato');
+
+Route::post(
+    '/contato',
+    [\App\Http\Controllers\ContatoController::class,
+    'salvar'
+])->name('site.contato');
+
+Route::get(
+    '/login',
+    function () {
+        return 'Login';
+})->name('site.login');
+
+
+// rotas privadas
+
+Route::prefix('/app')->group(function () {
+
+    Route::get('/clientes',
+        function () {
+            return 'clientes';
+        })->name('app.clientes');
+
+    Route::get('/fornecedores',
+        [FornecedorController::class, 'index'
+        ])->name('app.fornecedores');
+
+
+    Route::get('/produtos',
+        function () {
+            return 'produtos';
+        })->name('app.produtos');
+});
+
+
+
+Route::get('/teste/{p1}/{p2}',
+    [TesteController::class, 'teste'
+    ])->name('teste');
+
+
+// Rota de fallback
+Route::fallback(function () {
+    echo 'A rota acessada não existe. Clique <a href="'.route('site.index'
+    ).'"/>aqui</a> para ir para a página inicial';
+});
